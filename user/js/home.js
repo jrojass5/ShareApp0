28cfe85580bf68447a1147b9a095e72b5d54fef9
jrojass5 <rojassilvajefferson@gmail.com>
2023-11-02 18:49:47 -0500

    primer compromiso

--- user/js/home.js
+++ user/js/home.js
@@ -5,7 +5,5 @@ window.onload=function(){
     });
 }
 function seeadmin(){
-    const collectionRef = firebase.firestore().collection("Usuarios");
-const userCount = collectionRef.docCount();
-console.log(`Hay ${userCount} usuarios en la colección Usuarios.`);
+
 }
